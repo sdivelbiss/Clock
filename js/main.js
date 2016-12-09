602fc12dbb0a04021bfbf04a2b60e92d0037ba72
Scott Divelbiss <scottdivelbiss@Scotts-MacBook-Pro.local>
2016-12-08 20:21:16 -0800

    NewClock

--- js/main.js
+++ js/main.js
@@ -4,16 +4,40 @@ function startTime (){
 	var minutes = today.getMinutes()
 	var seconds = today.getSeconds()
 
+	
+	 // if (18 < hours||hours > 5) {
+  //      document.write("<link rel='stylesheet' href='../Clock/css/night.css' type='text/css'>");
+  //     }
+  //    if (18 < hours||hours > 5) {
+  //    	document.write("<link rel='stylesheet' href='../Clock/css/day.css' type='text/css'>");
+  //    }
+
+     //-----------------------Adding a class wouldn't work - had to add new stylesheets -----------------------
+	if (hours > 18 || hours < 5) {
+	document.getElementById("clock").className = "";
+	document.getElementById("clock").className = "clocknight";
+	}
+	else  {
+	document.getElementById("clock").className = "";
+	document.getElementById("clock").className = "clockday";
+	}
+	
+
+	
+
+
 	if (hours >= 13) {
 		hours = hours - 12
 	}
+
+	
 	
 
 
 	minutes = checkTime(minutes)
 	seconds = checkTime(seconds)
 
-	document.getElementById("clock").innerHTML = hours + ":" + minutes + ":" + seconds
+	document.getElementById("clock").innerHTML = hours + ":" + minutes + ":" + seconds;
 
  	setTimeout(function () {
         startTime()
